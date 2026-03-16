ac1a0a3e1c386bbe486dbbb09d221c24c1153a11
akshayshahasane <akshayshahasane2527@gmail.com>
2026-03-17 00:17:54 +0530

    expense backend update

--- Expense_Tracker/src/main/java/com/project/Expense_Tracker/controller/UserController.java
+++ Expense_Tracker/src/main/java/com/project/Expense_Tracker/controller/UserController.java
@@ -4,13 +4,11 @@ import com.project.Expense_Tracker.dto.LoginRequest;
 import com.project.Expense_Tracker.entity.User;
 import com.project.Expense_Tracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     @Autowired
